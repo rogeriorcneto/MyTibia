3835acfbf0b6e4fd22c8395dcab24922780e8360
rogeriorcneto <91757229+rogeriorcneto@users.noreply.github.com>
2022-07-30 05:33:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace NeoServer.Benchmarks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugBuildConfig());
+        // var summary = BenchmarkRunner.Run<ReadOnlySpanVsSpanAllocBenchmark>();
+
+        Console.ReadKey();
+    }
+}
